b5498d130de02b2042e6fb84b5158219f4c1927b
P@ <patmaynard452@gmail.com>
2023-07-25 15:17:15 -0400

    -Fixed a whoopsie where the element was just a function call and not a component...womp womp...
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/index';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path='/' element={Home} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </div>
     </Router>
