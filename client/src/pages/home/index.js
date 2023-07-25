61ba0a6b9c50441708d0b69c0b48372d50e3b5f6
P@ <patmaynard452@gmail.com>
2023-07-25 15:16:18 -0400

    -Fixed an auto fill whoopsie in referencing the styles
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,20 +1,22 @@
 import styles from './styles.module.css'
 
-export default Home = () => {
+const Home = () => {
     return(
-        <div className='styles.container'>
-            <div className='styles.formContainer'>
-                <h1 className='title'>{`Speak-EZ`}</h1>
-                <input className='styles.input' placeholder='Username' />
+        <div className={styles.container}>
+            <div className={styles.formContainer}>
+                <h1 className={styles.title}>{`Speak-EZ`}</h1>
+                <input className={styles.input} placeholder='Username' />
 
-                <select className='{styles.input}'>
+                <select className={styles.input}>
                     <option>-- Get a Room --</option>
                     <option value='1'>Room 1</option>
                     <option value='2'>Room 2</option>
                 </select>
 
-                <button className='btn btn-secondary'>Join Room</button>
+                <button className='btn btn-secondary' id={styles.joinBtn}>Join Room</button>
             </div>
         </div>
     );
 };
+
+export default Home;
