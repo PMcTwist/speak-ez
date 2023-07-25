88d2eb71962a28ffa9fc045e4f98f092b761c372
P@ <patmaynard452@gmail.com>
2023-07-25 15:41:25 -0400

    -Took in the new state objects and tied them to their front end objects
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,19 +1,43 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './styles.module.css'
 
-const Home = () => {
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+    const navigate = useNavigate();
+
+    const joinRoom = () => {
+        if (room !== '' && username !== '') {
+            socket.emit('join_room', { username, room });
+        }
+
+        navigate('/chat', { replace: true });
+    };
+
     return(
         <div className={styles.container}>
             <div className={styles.formContainer}>
                 <h1 className={styles.title}>{`Speak-EZ`}</h1>
-                <input className={styles.input} placeholder='Username' />
+                <input 
+                    className={styles.input} 
+                    placeholder='Username' 
+                    onChange={(e)=> setUsername(e.target.value)}
+                />
 
-                <select className={styles.input}>
+                <select 
+                    className={styles.input}
+                    onChange={(e)=> setRoom(e.target.value)}
+                >
                     <option>-- Get a Room --</option>
                     <option value='1'>Room 1</option>
                     <option value='2'>Room 2</option>
                 </select>
 
-                <button className='btn btn-secondary' id={styles.joinBtn}>Join Room</button>
+                <button 
+                    className='btn btn-secondary' 
+                    id={styles.joinBtn}
+                    onClick={joinRoom}
+                >
+                    Join Room
+                </button>
             </div>
         </div>
     );
