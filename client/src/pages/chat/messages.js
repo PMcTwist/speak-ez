af0754b37d5856694732ec3bd75cca0d0ae66d6f
P@ <patmaynard452@gmail.com>
2023-07-26 11:06:33 -0400

    -Implemented a component for showing messages
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

---
+++ client/src/pages/chat/messages.js
@@ -0,0 +1,50 @@
+import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+const Messages = ({ socket }) => {
+    const [messagesRecieved, setMessagesRecieved] = useState([]);
+
+    // Whenever a socket event is fired on the server
+    useEffect(() => {
+        socket.on('receive_message', (data) => {
+            console.log(data);
+            setMessagesRecieved((state) => [
+                ...state,
+                {
+                    message: data.message,
+                    username: data.username,
+                    __createdTime__: data.__createdTime__,
+                },
+            ])
+        })
+
+        // Remove Listener
+        return () => socket.off('reveive_message');
+    }, [socket]);
+
+    function formatDataFromTimestamp(timestamp) {
+        const date = new Date(timesstamp);
+        return date.toLocaleString();
+    }
+
+    // Output the actual component
+    return (
+        <div className={styles.messageColumn}>
+            {messagesRecieved.map((msg, i) => (
+                <div className={styles.message} key={i}>
+                    <div className={styles.msgMetaContainer}>
+                        <span className={styles.msgMeta}>{msg.username}</span>
+                        <span className={styles.msgMeta}>
+                            {formatDataFromTimestamp(msg.__createdTime__)}
+                        </span>
+                    </div>
+                    <p className={styles.msgText}>{msg.message}</p>
+                    <br />
+                </div>
+            ))}
+        </div>
+    )
+
+};
+
+export default Messages
