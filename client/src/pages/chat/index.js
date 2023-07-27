91b0d77ef7118bdaac65c787b4c1f534035da96e
P@ <patmaynard452@gmail.com>
2023-07-27 15:22:02 -0400

    -Added the room and users component to the index of the chat page
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- client/src/pages/chat/index.js
+++ client/src/pages/chat/index.js
@@ -1,12 +1,19 @@
 import styles from './styles.module.css';
 
 // Import our components
+import RoomAndUsers from './room-and-users';
 import MessagesReceived from './messages';
 import SendMessage from './send-message';
 
 const Chat = ({ username, room, socket }) => {
     return (
         <div className={styles.chatContainer}>
+            <RoomAndUsers 
+                socket={socket} 
+                username={username} 
+                room={room} 
+            />
+
             <div>
                 <MessagesReceived 
                     socket={socket} 
